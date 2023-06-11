5dcfbc9e2c7050c2ceb5c7dfc2b39334c80826ec
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-11 12:40:26 +0100

    added model relationships

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -10,5 +10,12 @@ class Application extends Model
     use HasFactory;
     protected $guarded = ['id'];
     protected $table = 'application_form';
-
+    public function state()
+    {
+        return $this->belongsTo(State::class, 'stateid');
+    }
+    public function lga()
+    {
+        return $this->belongsTo(Lga::class, 'lgaid');
+    }
 }
