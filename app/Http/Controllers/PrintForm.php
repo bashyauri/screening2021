facb9c52fee48e4f40a016b7d7b91a913a805ca8
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-11 10:31:28 +0100

    made correction to program id

--- app/Http/Controllers/PrintForm.php
+++ app/Http/Controllers/PrintForm.php
@@ -25,42 +25,42 @@ class PrintForm extends Controller
         //Bio Section
         $user = auth()->user()->account_id;
         $jambNo = auth()->user()->jambno;
-        $statuses= Status::where('account_id','=',$user)->sole();
-        $applicationDetails= Application::where('account_id','=',$user)->sole();
-        $lga = Lga::where('id','=',$applicationDetails->lgaid)->first();
-        $state = State::where('id','=',$applicationDetails->stateid)->first();
-        $program = Programme::where('id','=',$applicationDetails->programme_id)->first();
-        $proposedCourse = ProposedCourse::where('account_id','=',$user)->sole();
-        $courses = Course::where('id','=',$proposedCourse->course_id)->first();
+        $statuses = Status::where('account_id', '=', $user)->sole();
+        $applicationDetails = Application::where('account_id', '=', $user)->sole();
+        $lga = Lga::where('id', '=', $applicationDetails->lgaid)->first();
+        $state = State::where('id', '=', $applicationDetails->stateid)->first();
+        $program = Programme::where('id', '=', auth()->user()->programme_id)->first();
+        $proposedCourse = ProposedCourse::where('account_id', '=', $user)->sole();
+        $courses = Course::where('id', '=', $proposedCourse->course_id)->first();
         $transaction = Transaction::where('account_id', '=', $user)
-        ->where('resource', '=', 'Admission Screening Fees')
-        ->where('use_status', '=', '(Not Used)')
-        ->where(function ($query) {
+            ->where('resource', '=', 'Admission Screening Fees')
+            ->where('use_status', '=', '(Not Used)')
+            ->where(function ($query) {
                 $query->where('status', '01')
                     ->orWhere('status', '00');
             })->first();
 
 
-        $institutions = Institution::where('account_id','=',$user)->get();
-        $exams = ExamDetail::where('account_id','=',$user)->get();
-        $subjects = ExamGrade::where('account_id','=',$user)->get();
+        $institutions = Institution::where('account_id', '=', $user)->get();
+        $exams = ExamDetail::where('account_id', '=', $user)->get();
+        $subjects = ExamGrade::where('account_id', '=', $user)->get();
 
 
-        if($statuses->status==6){
+        if ($statuses->status == 6) {
             $data = array(
                 'programme' => ucwords($program->name),
-                'fullName'=>$applicationDetails->surname.' '.$applicationDetails->firstname.' '.$applicationDetails->m_name,
-                'serialNo'=>$applicationDetails->sN,
-                'rrr'=>$transaction->RRR,
+                'fullName' => $applicationDetails->surname . ' ' . $applicationDetails->firstname . ' ' . $applicationDetails->m_name,
+                'serialNo' => $applicationDetails->sN,
+                'rrr' => $transaction->RRR,
                 'accountId' => $applicationDetails->account_id,
-                'passport'=>$applicationDetails->filename,
-                'dob'=>$applicationDetails->d_birth,
-                'gender'=>ucwords($applicationDetails->gender),
-                'homeTown'=>$applicationDetails->home_town,
-                'lga'=>$lga->name,
-                'state'=>$state->name,
+                'passport' => $applicationDetails->filename,
+                'dob' => $applicationDetails->d_birth,
+                'gender' => ucwords($applicationDetails->gender),
+                'homeTown' => $applicationDetails->home_town,
+                'lga' => $lga->name,
+                'state' => $state->name,
                 'nationality' => $applicationDetails->nationality,
-                'maritalStatus' =>ucwords($applicationDetails->marital_status),
+                'maritalStatus' => ucwords($applicationDetails->marital_status),
                 'phoneNumber' => ucwords($applicationDetails->p_number),
                 'homeAddress' => ucwords($applicationDetails->home_address),
                 'correspondentAddress' => ucwords($applicationDetails->cor_address),
@@ -69,24 +69,20 @@ class PrintForm extends Controller
                 'nextkinGsm' => ucwords($applicationDetails->nextkin_gsm),
                 'nextkinAddress' => ucwords($applicationDetails->nextkin_address),
                 'institutions' => $institutions,
-                'count'=>0,
-                'exams'=>$exams,
-                'subjects'=>$subjects,
-                'subjectCount'=>0,
-                'jambNumber'=> $jambNo,
-                'score' =>$proposedCourse->score,
-                'course'=>$courses->course_name,
-                'passport'=>$applicationDetails->filename,
+                'count' => 0,
+                'exams' => $exams,
+                'subjects' => $subjects,
+                'subjectCount' => 0,
+                'jambNumber' => $jambNo,
+                'score' => $proposedCourse->score,
+                'course' => $courses->course_name,
+                'passport' => $applicationDetails->filename,
 
             );
             return view('print-form')->with($data);
-
-        }
-        else
-        {
+        } else {
             return Redirect('profile');
         }
-
     }
     public function printOffer()
     {
@@ -100,22 +96,22 @@ class PrintForm extends Controller
         $program = Programme::where('id', '=', $applicationDetails->programme_id)->first();
         $proposedCourse = ProposedCourse::where('account_id', '=', $user)->sole();
         $courses = Course::where('id', '=', $proposedCourse->course_id)->first();
-        $departments =Department::where('id', '=', $proposedCourse->department_id)->first();
-        
-       
-            
-   
+        $departments = Department::where('id', '=', $proposedCourse->department_id)->first();
+
+
+
+
         $institutions = Institution::where('account_id', '=', $user)->get();
         $exams = ExamDetail::where('account_id', '=', $user)->get();
         $subjects = ExamGrade::where('account_id', '=', $user)->get();
-         $transaction = Transaction::where('account_id', '=', $user)
+        $transaction = Transaction::where('account_id', '=', $user)
             ->where('resource', '=', 'Acceptance Fees')
             ->where('use_status', '=', '(Not Used)')
             ->where(function ($query) {
                 $query->where('status', '01')
                     ->orWhere('status', '00');
             })->first();
-        
+
         if ($statuses->status == 6 && $transaction != null) {
             $data = array(
                 'programme' => ucwords($program->name),
@@ -146,17 +142,15 @@ class PrintForm extends Controller
                 'jambNumber' => $jambNo,
                 'score' => $proposedCourse->score,
                 'course' => $courses->course_name,
-                 'department' => $departments->department_name,
+                'department' => $departments->department_name,
                 'passport' => $applicationDetails->filename,
-                 'appliedProgram' => $applicationDetails->programme_id,
+                'appliedProgram' => $applicationDetails->programme_id,
 
             );
             return view('offer')->with($data);
-
         } else {
             return Redirect('dashboard');
         }
-
     }
     //
 
