aa819dfb26a0ef722154851b42ddcb9bfa017b54
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-14 15:32:13 +0100

    createa admin controller

---
+++ app/Http/Controllers/AdminAuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('admin.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::guard('admin')->attempt($credentials)) {
+            // Authentication successful
+            return redirect()->intended('/admin/dashboard');
+        } else {
+            // Authentication failed
+            return back()->withErrors(['email' => 'Invalid credentials']);
+        }
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect('/admin/login');
+    }
+}
