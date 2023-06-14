ee21bd48a1e76439fa966425f4a04fd3cdc2c168
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-14 15:29:38 +0100

    delete migrations

---
+++ app/Models/Admin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
