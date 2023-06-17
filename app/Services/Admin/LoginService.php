3abffe9bb114926cd785d0caaee5425b8b811808
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-17 16:13:23 +0100

    added Login Request

---
+++ app/Services/Admin/LoginService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services\Admin;
+
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Class LoginService.
+ */
+class LoginService
+{
+    public function login($credentials){
+        return Auth::guard('admin')->attempt($credentials);
+    }
+}
