49cb118b439f765ba905f734d7dfd8bb24e068c9
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-17 17:24:40 +0100

    update admin route

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function dashboard()
     {
         dd('admin');
+    }
+    public function logout(): RedirectResponse
+    {
+        Auth::guard('admin')->logout();
+        return redirect('/admin/login');
     }
 }
