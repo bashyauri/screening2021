ccb0ca110bbfaf621ef532dd5038a7fe0bf48d2f
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-17 14:34:54 +0100

    create admin_role table

---
+++ database/migrations/2023_06_17_120009_create_admin_role_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_role', function (Blueprint $table) {
+            $table->foreignIdFor(\App\Models\Admin::class);
+            $table->foreignIdFor(\App\Models\Role::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_role');
+    }
+}
