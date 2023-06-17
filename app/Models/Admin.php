d25770f1064026b281e13413497f59ef2be20eef
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-17 14:32:07 +0100

    Add Admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -10,5 +10,10 @@ class Admin extends Authenticatable
     use Notifiable;
 
     protected $table = 'admins';
-    // ...
+    protected $guarded=['id'];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'admin_role');
+    }
 }
