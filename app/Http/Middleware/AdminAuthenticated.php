ca68eaf68d53342639a89d06103976ab718822ee
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-18 19:25:51 +0100

    update

--- app/Http/Middleware/AdminAuthenticated.php
+++ app/Http/Middleware/AdminAuthenticated.php
@@ -21,7 +21,7 @@ class AdminAuthenticated
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
             } else {
-                return redirect(route('adminLogin'));
+                return redirect(route('admin.login'));
             }
         }
 
