b01024844d4730bd591cb807284beb46ed92ef82
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-18 10:54:47 +0100

    added getAdminDetails function

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -40,6 +40,14 @@ class AdminController extends Controller
 
 
         return view('admin.dashboard')->with($data);
+    }
+    public function getAdminDetails()
+    {
+
+        return view('admin.settings.update_admin_details');
+    }
+    public function updateAdminDetails()
+    {
     }
     public function logout(): RedirectResponse
     {
