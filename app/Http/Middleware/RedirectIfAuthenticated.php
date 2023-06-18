2cb14693b480c84ba3c3097a082a28617fbba36d
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-18 19:26:21 +0100

    check if guard is administrator

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,21 +22,21 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            // if (Auth::guard($guard)->check()) {
-            //     return redirect(RouteServiceProvider::HOME);
-            // }
-            switch ($guard) {
-                case 'admin':
-                    if (Auth::guard($guard)->check()) {
-                        return redirect()->route('admin.dashboard');
-                    }
-                    break;
-                default:
-                    if (Auth::guard($guard)->check()) {
-                        return redirect()->route('home');
-                    }
-                    break;
+            if (Auth::guard($guard)->check()) {
+                return redirect(RouteServiceProvider::HOME);
             }
+            // switch ($guard) {
+            //     case 'admin':
+            //         if (Auth::guard($guard)->check()) {
+            //             return redirect()->route('admin.dashboard');
+            //         }
+            //         break;
+            //     default:
+            //         if (Auth::guard($guard)->check()) {
+            //             return redirect()->route('home');
+            //         }
+            //         break;
+            // }
             return $next($request);
         }
     }
