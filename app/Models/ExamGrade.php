ce022f00443c7a00f783b4d745c09d2d4771e71b
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-20 17:05:01 +0100

    add relationship

--- app/Models/ExamGrade.php
+++ app/Models/ExamGrade.php
@@ -9,4 +9,8 @@ class ExamGrade extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function application()
+    {
+        return $this->belongsTo(Application::class, 'account_id');
+    }
 }
