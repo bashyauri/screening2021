ec16493ebaaa2fce0ca96466f8b017cd9d2ab9e3
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-21 19:36:48 +0100

    create applicant controller

---
+++ app/Http/Controllers/Admin/ApplicantController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApplicantController extends Controller
+{
+    public function recommend(Request $request)
+    {
+    }
+}
