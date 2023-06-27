0645f6eb3b76236a3a6be07030d5b2b0b24e36cd
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-27 20:10:20 +0100

    add Request

--- app/Http/Controllers/Admin/ApplicantController.php
+++ app/Http/Controllers/Admin/ApplicantController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\RecommendApplicanRequest;
 use App\Models\Application;
+use App\Services\Admin\ApplicantService;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,17 +13,14 @@ use Illuminate\Support\Facades\Log;
 
 class ApplicantController extends Controller
 {
-    public function recommend(Request $request)
+    public function __construct(protected ApplicantService $applicantService)
+    {
+    }
+    public function recommend(RecommendApplicanRequest $request)
     {
         try {
-            Application::where(['account_id' => $request->accountId])->update(
-                [
-                    'department_id' => Auth::guard('admin')->user()->department_id,
-                    'remark' => 'Qualify for Admission',
-                    'recommendation' => $request->criteria,
-                    'comment' => $request?->comments
-                ]
-            );
+            $this->applicantService->recommend($request->validated());
+
             return  redirect()->back()->with(['success_message' => 'Password  Updated']);
         } catch (Exception $e) {
             Log::alert($e->getMessage());
