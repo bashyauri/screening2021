42146155a814ac2da27b6818670bdde9d9f19db0
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-27 20:10:53 +0100

    create applicant service

---
+++ app/Services/Admin/ApplicantService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services\Admin;
+
+use App\Models\Application;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Class ApplicantService.
+ */
+class ApplicantService
+{
+    public function recommend(array $data)
+    {
+        Application::where(['account_id' => $data['accountId']])->update(
+            [
+                'department_id' => Auth::guard('admin')->user()->department_id,
+                'remark' => 'Qualify for Admission',
+                'recommendation' => $data['criteria'],
+                'comment' => $data['comments'] ?? null,
+            ]
+        );
+    }
+}
