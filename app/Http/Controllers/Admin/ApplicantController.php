03d24da9158b71a1d43d2c34fe61f33dfa264d93
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-27 19:01:07 +0100

    added recommend functionality

--- app/Http/Controllers/Admin/ApplicantController.php
+++ app/Http/Controllers/Admin/ApplicantController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Application;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
@@ -12,28 +13,19 @@ class ApplicantController extends Controller
 {
     public function recommend(Request $request)
     {
-
-        // Access the submitted data using $request object
-
-
-        // Log the data
-        Log::info('Criteria: ' . $request->criteria);
-        Log::info('Comments: ' . $request->comments);
-        Log::info('Recommend: ' . $request->recommend);
-        Log::info('Account ID: ' .   $request->input('accountId'));
-
-        // Perform the necessary actions with the received data
-        // ...
-
-        // Return a response (optional)
-        return response()->json(['message' => 'Data received and processed successfully']);
-
-
-
-
-        Application::where(['account_id' => $request->recommend])->update([
-            'department_id' => Auth::guard('admin')->user()->department_id,
-            'remark' => 'Qualify for Admission',
-        ]);
+        try {
+            Application::where(['account_id' => $request->accountId])->update(
+                [
+                    'department_id' => Auth::guard('admin')->user()->department_id,
+                    'remark' => 'Qualify for Admission',
+                    'recommendation' => $request->criteria,
+                    'comment' => $request?->comments
+                ]
+            );
+            return  redirect()->back()->with(['success_message' => 'Password  Updated']);
+        } catch (Exception $e) {
+            Log::alert($e->getMessage());
+            return redirect()->back()->withErrors(['error_message' => 'Something went wrong']);
+        }
     }
 }
