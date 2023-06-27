3edc84aa3b2d216df5e3ead9e5292af76e8978b0
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-27 19:01:28 +0100

    add error message

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -730,6 +730,26 @@
                     <div class="card">
                         <div class="card-body">
                             <p class="card-title">Advanced Table</p>
+                            @if (Session::has('error_message'))
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <strong>Error</strong> {{ Session::get('error_message') }}
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                            @endif
+
+                            @if ($errors->any())
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+
+                                </div>
+                            @endif
                             <div class="row">
                                 <div class="col-12">
                                     <div class="table-responsive">
