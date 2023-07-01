553e35858380a276ae9accaf07dd49f202bb1131
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-07-01 20:44:14 +0100

    add route

--- app/Services/Admin/Reports/ApplicantsService.php
+++ app/Services/Admin/Reports/ApplicantsService.php
@@ -61,7 +61,55 @@ class ApplicantsService
         });
         return $applicants;
     }
-    public function convertToDocx($html)
+    public function getRecommendedApplicants()
     {
+        $applicants = DB::table('application_form')
+            ->join('proposed_courses', 'application_form.account_id', 'proposed_courses.account_id')
+            ->join('tb_accountcreation', 'application_form.account_id', 'tb_accountcreation.account_id')
+            ->join('courses', 'courses.id', 'proposed_courses.course_id')
+            ->join('states', 'states.id', 'application_form.stateid')
+            ->join('lgas', 'lgas.id', 'application_form.lgaid')
+            ->leftJoin('exam_grades', 'exam_grades.account_id', 'application_form.account_id')
+            ->where('application_form.department_id', '=',  Auth::guard('admin')->user()->department_id)
+            ->where('application_form.remark', '=', 'Qualify for Admission')
+            ->select(
+                'application_form.account_id',
+                'application_form.surname',
+                'application_form.firstname',
+                'application_form.m_name',
+                'application_form.p_number',
+                'application_form.remark',
+
+                'courses.course_name',
+                'proposed_courses.score',
+                'tb_accountcreation.jambno',
+                'states.name',
+                'lgas.name as lga'
+            )
+            ->groupBy('application_form.account_id')
+            ->groupBy('application_form.remark')
+            ->groupBy('application_form.surname')
+            ->groupBy('application_form.firstname')
+            ->groupBy('application_form.m_name')
+            ->groupBy('application_form.p_number')
+            ->groupBy('courses.course_name')
+            ->groupBy('proposed_courses.score')
+            ->groupBy('tb_accountcreation.jambno')
+            ->groupBy('states.name')
+            ->groupBy('lga')
+
+            ->orderBy('proposed_courses.course_id', 'asc')
+            ->get();
+
+        $applicantIds = $applicants->pluck('account_id')->toArray();
+        $examGrades = DB::table('exam_grades')
+            ->whereIn('account_id', $applicantIds)
+            ->get();
+
+        $applicants = $applicants->map(function ($applicant) use ($examGrades) {
+            $applicant->exam_grades = $examGrades->where('account_id', $applicant->account_id);
+            return $applicant;
+        });
+        return $applicants;
     }
 }
