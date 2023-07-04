4a068c9f4ae56a5184dc87be90376972b5ea5211
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-07-04 10:11:04 +0100

    add search form

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -93,100 +93,34 @@
             </div>
 
 
-            @if (count($numberOfCourses) > 1)
+            @if (count($courses) > 1)
                 <div class="row">
                     <div class="col-md-8 stretch-card grid-margin">
                         <div class="card">
                             <div class="card-body">
-                                <h4 class="card-title">Inline forms</h4>
+                                <h4 class="card-title">Search Course</h4>
                                 <p class="card-description">
                                     Use the <code>.form-inline</code> class to display a series of labels, form controls,
                                     and buttons on a single horizontal row
                                 </p>
                                 <form class="form-inline">
-                                    <label class="sr-only" for="inlineFormInputName2">Name</label>
-                                    <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2"
+                                    <label class="sr-only" for="inlineFormInputName2">Courses</label>
+                                    <select class="form-control mb-6 mr-sm-4" id="inlineFormInputName2"
                                         placeholder="Jane Doe">
+                                        <option value="" selected>Select Courses</option>
+                                        @foreach ($courses as $course)
+                                            <option value="{{ $course->id }}">{{ $course->course_name }}</option>
+                                        @endforeach
 
-                                    <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
-                                    <div class="input-group mb-2 mr-sm-2">
-                                        <div class="input-group-prepend">
-                                            <div class="input-group-text">@</div>
-                                        </div>
-                                        <input type="text" class="form-control" id="inlineFormInputGroupUsername2"
-                                            placeholder="Username">
-                                    </div>
-                                    <div class="form-check mx-sm-2">
-                                        <label class="form-check-label">
-                                            <input type="checkbox" class="form-check-input" checked="">
-                                            Remember me
-                                            <i class="input-helper"></i></label>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                                    </select>
+
+                                    <button type="submit" class="btn btn-primary mb-2">Search</button>
                                 </form>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-md-4 stretch-card grid-margin">
-                        <div class="card">
-                            <div class="card-body">
-                                <p class="card-title">Notifications</p>
-                                <ul class="icon-data-list">
-                                    <li>
-                                        <div class="d-flex">
-                                            <img src="images/faces/face1.jpg" alt="user">
-                                            <div>
-                                                <p class="text-info mb-1">Isabella Becker</p>
-                                                <p class="mb-0">Sales dashboard have been created</p>
-                                                <small>9:30 am</small>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="d-flex">
-                                            <img src="images/faces/face2.jpg" alt="user">
-                                            <div>
-                                                <p class="text-info mb-1">Adam Warren</p>
-                                                <p class="mb-0">You have done a great job #TW111</p>
-                                                <small>10:30 am</small>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="d-flex">
-                                            <img src="images/faces/face3.jpg" alt="user">
-                                            <div>
-                                                <p class="text-info mb-1">Leonard Thornton</p>
-                                                <p class="mb-0">Sales dashboard have been created</p>
-                                                <small>11:30 am</small>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="d-flex">
-                                            <img src="images/faces/face4.jpg" alt="user">
-                                            <div>
-                                                <p class="text-info mb-1">George Morrison</p>
-                                                <p class="mb-0">Sales dashboard have been created</p>
-                                                <small>8:50 am</small>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="d-flex">
-                                            <img src="images/faces/face5.jpg" alt="user">
-                                            <div>
-                                                <p class="text-info mb-1">Ryan Cortez</p>
-                                                <p class="mb-0">Herbs are fun and easy to grow.</p>
-                                                <small>9:00 am</small>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
             @else
                 <div class="row">
