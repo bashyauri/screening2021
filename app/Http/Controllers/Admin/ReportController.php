e8f6a861f7c7ea541a362437bc00cb67c098aec9
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-07-11 13:55:51 +0100

    added report

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Application;
 use App\Models\Department;
+use App\Models\ProposedCourse;
 use App\Services\Admin\Reports\ApplicantsService;
 use App\Services\Admin\ReportService;
 use Exception;
@@ -26,5 +27,17 @@ class ReportController extends Controller
             Log::info("Something went wrong: " . $e->getMessage());
             return redirect()->back()->with(['error_message' => 'Something went wrong. Please contact CIT.']);
         }
+    }
+    public function numberOfApplicants($department = 0)
+    {
+        return ProposedCourse::where(['department_id' => $department])->count();
+    }
+    public function numberOfRecommendedApplicants($department = 0)
+    {
+        return Application::where(['department_id' => $department, 'remark' => 'Qualify for Admission'])->count();
+    }
+    public function numberOfShortlistedApplicants($department = 0)
+    {
+        return Application::where(['department_id' => $department, 'remark' => 'shortlisted'])->count();
     }
 }
