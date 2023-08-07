37892298c0fd1dc77326fc88271e65d1a069a871
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-08-07 17:53:53 +0100

    update footer

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -1,6 +1,9 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-      <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021.  Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
-      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
+        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright CIT Wufpbk ©
+            {{ now()->year }}.<a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from
+            08038272560. All rights reserved.</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">08038272560<i
+                class="ti-heart text-danger ml-1"></i></span>
     </div>
-  </footer>
+</footer>
